c13855cfc26b52dccb3ec42f7e9306ec424e9177
Lavanya-63 <lavanyadhavamani63@gmail.com>
2022-12-09 16:00:45 +0530

    create constant

---
+++ WedLock/Utility/constant.js
@@ -0,0 +1,33 @@
+export const COLOR = {
+ BLACK:"#000000",
+ ORANGE:"#DF6229",
+ WHITE:"#FFFFFF",
+ LIGHT_ORANGE:"#F77D3B"
+}
+export const BACKGROUND_COLOUR = {
+    ORANGE:"#F77D3B",
+    WHITE:"#FFFFFF",
+    BLACK:"#000000"
+}
+//tab screen icon name
+export const TAB_NAME ={
+    HOME:"Home",
+    CHECKLIST:"Checklist",
+    USER:"User"
+}
+// section header of home page 
+export const HOME_SECTION_HEADER ={
+    ENGAGEMENT:" For Engagement Ceremony Decorations",
+    SANGEET:"For Sangeet Ceremony Decorations",
+    HALDI:"For Haldi Ceremony Decorations",
+    MEHANDI:"For Mehandi Ceremony Decorations",
+    WEDDING:"For Wedding Day Decorations",
+    OTHERS:"Other Arrangements"
+}
+//field button in profile
+export const PROFILE_FIELD={
+    PROFILE:"Profile details",
+    FEEDBACK:"Feddback",
+    ABOUT_US:"About Us",
+    LOG_OUT:"Logout"
+}
