260c892745215d624918fc22a90b872e40d600ca
hari2621 <h.k.hari2621@gmail.com>
2022-12-09 16:06:29 +0530

    constant merge

---
+++ WedLock/Utility/constant.js
@@ -0,0 +1,58 @@
+export const NAME_LOGIN={
+    WELCOME:"Welcome To ",
+    APP_NAME:"TOFO ",
+    TYPE:"Events",
+    USERNAME:"Username",
+    PASSWORD:"Password",
+    LOGIN_TEXT:"Login",
+    FORGOT:"Forgot Password?",
+    SIGNUP:"Sign Up",
+    NEW_ACCOUNT:"Don't have an account?",
+    USERNAME_ERROR:"UserName is not valid",
+    PASSWORD_ERROR:"Password is not valid"
+}
+
+export const NAVIGATION_SCREENS={
+    LOGIN:"Login",
+    HOME_SCREEN:"BottomNavigator",
+    FORGOT_SCREEN:"Forgot",
+    SIGN_UP:"SignUp",
+
+}
+
+export const COLOR = {
+ BLACK:"#000000",
+ ORANGE:"#DF6229",
+ WHITE:"#FFFFFF",
+ LIGHT_ORANGE:"#F77D3B",
+ RED:'red',
+ ORANGE:'orangered',
+ BLUE:'blue'
+}
+export const BACKGROUND_COLOUR = {
+    ORANGE:"#F77D3B",
+    WHITE:"#FFFFFF",
+    BLACK:"#000000"
+}
+//tab screen icon name
+export const TAB_NAME ={
+    HOME:"Home",
+    CHECKLIST:"Checklist",
+    USER:"User"
+}
+// section header of home page 
+export const HOME_SECTION_HEADER ={
+    ENGAGEMENT:" For Engagement Ceremony Decorations",
+    SANGEET:"For Sangeet Ceremony Decorations",
+    HALDI:"For Haldi Ceremony Decorations",
+    MEHANDI:"For Mehandi Ceremony Decorations",
+    WEDDING:"For Wedding Day Decorations",
+    OTHERS:"Other Arrangements"
+}
+//field button in profile
+export const PROFILE_FIELD={
+    PROFILE:"Profile details",
+    FEEDBACK:"Feddback",
+    ABOUT_US:"About Us",
+    LOG_OUT:"Logout"
+}
