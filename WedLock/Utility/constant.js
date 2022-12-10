f803d2ffc47e50f62417dd6c4b90d9c46ef93be0
Lavanya-63 <lavanyadhavamani63@gmail.com>
2022-12-10 13:03:45 +0530

    corrections in signup page

--- WedLock/Utility/constant.js
+++ WedLock/Utility/constant.js
@@ -1,32 +1,49 @@
-//Forgot page Error
+export const BACKGROUND_COLORS={
+    ORANGE:"orangered",
+    PINK:"#FAAFBA",
+    GREY:"rgba(52,52, 52, 0.9)",
+    WHITE:"#FFFFFF",
+    ORANGE_:"#F77D3B",
+    BLACK:"#000000"
+}
+export const COLOR = {
+    BLACK:"#000000",
+    ORANGE:"#DF6229",
+    WHITE:"#FFFFFF",
+    LIGHT_ORANGE:"#F77D3B",
+    RED:'red',
+    ORANGE:'orangered',
+    BLUE:'blue',
+    ORANGERED:"orangered",
+   }
+//Error Message
 export const ERROR_MESSAGE={
     EMAIL_INVALID:"Enter valid Email Id",
     PASSWORD_INVALID:"Password must be One Upper,Lowercase,& 8digit",
     PASSWORD_CONFIRM:"Password must be same",
-    ENTER_EMAILID:"Enter your Email id.",
     ENTER_NEWPASSWORD:"New Password",
-    ENTER_CONFIRMPASSWORD:"Confirm New Password"
-
+    ENTER_CONFIRMPASSWORD:"Confirm New Password",
+    USERNAME:"Name is required",
+    PASSWORD:"Please enter the valid password(Eg:Aaa@12)",
+    CONFIRM_PASSWORD:"Password doesn't match",
+    MOBILE_NUMBER:"Enter valid number"
 }
-//Forgot page Color
-export const FORGOT_COLOR={
-   BLACK:"#000000",
-   RED:"red",
-   WHITE:"white",
-   ORANGERED:"orangered",
-   BACKGROUND_INPUT:"white"
+//page Title
+export const PAGE_CONTENT={
+        WELCOME:"Welcome To ",
+        APP_NAME:"TOFO ",
+        TYPE:"Events",
+        USERNAME:"Username",
+        PASSWORD:"Password",
+        LOGIN_TEXT:"Login",
+        FORGOT:"Forgot Password?",
+        CHANGEPASSWORD_TITLE:"Change Password",
+        SIGNUP:"Sign Up",
+        NEW_ACCOUNT:"Don't have an account?",
+        BACK_TO_LOGIN:"Back To Login",
+        CREATE:"Create"
 }
-//Forgot page Title
-export const FORGOT_TITLE={
-    TITLE_TEXT:"TOFO",
-    TITLE_CONTENT:"Change Password"
-}
-export const FORGOT_VALIDATION ={
-    EMAIL:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-}
-export const TITLE={
-    TITLETEXT:"TOFO",
-}
+//placeholder inputbox
 export const PLACEHOLDER={
     USERNAME:"User name",
     PASSWORD:"Password",
@@ -34,59 +51,12 @@ export const PLACEHOLDER={
     EMAIL:"Email",
     MOBILE_NUMBER:"Mobile Number"
 }
-export const BUTTONS={
-    BACK_TO_LOGIN:"Back To Login",
-    CREATE:"Create",
-}
-export const ERRORMESSAGE={
-    USERNAME:"Name is required",
-    PASSWORD:"Please enter the valid password(Eg:Aaa@12)",
-    CONFIRM_PASSWORD:"Password doesn't match",
-    EMAIL:"Enter valid Email",
-    MOBILE_NUMBER:"Enter valid number"
-}
-export const BACKGROUND_COLORS={
-    ORANGE:"orangered",
-    PINK:"#FAAFBA",
-    GREY:"rgba(52,52, 52, 0.9)",
-    WHITE:"#FFFFFF"
-}
-
-export const NAME_LOGIN={
-    WELCOME:"Welcome To ",
-    APP_NAME:"TOFO ",
-    TYPE:"Events",
-    USERNAME:"Username",
-    PASSWORD:"Password",
-    LOGIN_TEXT:"Login",
-    FORGOT:"Forgot Password?",
-    SIGNUP:"Sign Up",
-    NEW_ACCOUNT:"Don't have an account?",
-    USERNAME_ERROR:"UserName is not valid",
-    PASSWORD_ERROR:"Password is not valid"
-}
-
+//navigation screens 
 export const NAVIGATION_SCREENS={
     LOGIN:"Login",
     HOME_SCREEN:"BottomNavigator",
     FORGOT_SCREEN:"Forgot",
     SIGN_UP:"SignUp",
-
-}
-
-export const COLOR = {
- BLACK:"#000000",
- ORANGE:"#DF6229",
- WHITE:"#FFFFFF",
- LIGHT_ORANGE:"#F77D3B",
- RED:'red',
- ORANGE:'orangered',
- BLUE:'blue'
-}
-export const BACKGROUND_COLOUR = {
-    ORANGE:"#F77D3B",
-    WHITE:"#FFFFFF",
-    BLACK:"#000000"
 }
 //tab screen icon name
 export const TAB_NAME ={
@@ -130,10 +100,12 @@ export const DEMO_IMAGE={
     OTHERS1:"https://secureservercdn.net/45.40.150.54/ihr.09f.myftpupload.com/wp-content/uploads/2018/01/best-south-indian-food-wedding-lunch.jpg?time=1659995476",
     OTHERS2:"https://antphotography.in/wp-content/uploads/2019/08/Aditya-and-Chandani-fantastic-pre-wedding-photoshoot-in-Alibaug-and-Tamini-Ghat-Pune23.jpg",
 }
+//Regex validation
 export const REGEX = {
     WHITESPACE:/\S+@\S+\.\S+/,
     EMAIL:/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
     USERNAME: /^[a-zA-Z]{3,16}$/,
     PASSWORD:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
     NUMBER:/^[0-9]{10}$/,
+    EMAIL_FORGOT:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 }
