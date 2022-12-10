1d6de0f0b0ac34c742a4c3001f035ee61f7f76b2
Lavanya-63 <lavanyadhavamani63@gmail.com>
2022-12-10 16:36:41 +0530

    changes

---
+++ WedLock/Source/Navigation/BottomNavigation.js
@@ -0,0 +1,62 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Checklist from '../Screen/DashBoard/Checklist';
+import Profile from '../Screen/DashBoard/Profile';
+import Home from "../Screen/DashBoard/Home";
+import { BACKGROUND_COLOUR } from '../../Utility/Constant';
+import { TAB_NAME } from '../../Utility/Constant';
+const Tab = createBottomTabNavigator();
+
+export default function BottomNavigator() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+    >
+      <Tab.Screen
+        name={TAB_NAME.HOME}
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarLabelStyle: { color: BACKGROUND_COLOUR.BLACK, fontSize: 14 },
+          tabBarStyle: { backgroundColor: BACKGROUND_COLOUR.ORANGE },
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name={TAB_NAME.HOME} color={BACKGROUND_COLOUR.BLACK} size={28} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={TAB_NAME.CHECKLIST}
+        component={Checklist}
+        options={{
+          tabBarLabel: 'Checklist',
+          tabBarLabelStyle: { color: BACKGROUND_COLOUR.BLACK, fontSize: 14 },
+          tabBarStyle: { backgroundColor: BACKGROUND_COLOUR.ORANGE },
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name={TAB_NAME.BELL} color={BACKGROUND_COLOUR.BLACK} size={28} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={TAB_NAME.USER}
+        component={Profile}
+        options={{
+          tabBarLabel: 'User',
+          tabBarLabelStyle: { color: BACKGROUND_COLOUR.BLACK, fontSize: 14, },
+          tabBarStyle: { backgroundColor: BACKGROUND_COLOUR.ORANGE },
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name={TAB_NAME.ACCOUNT} color={BACKGROUND_COLOUR.BLACK} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+
+
+
+
+
+
+
+
---
+++ WedLock/Source/Screen/DashBoard/Profile.js
@@ -0,0 +1,77 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ImageBackground
+} from 'react-native';
+import {
+  BACKGROUND_COLORS,
+  PROFILE_FIELD,
+  NAVIGATION_SCREENS
+} from '../../../Utility/Constant';
+
+export default function Profile({ navigation }) {
+  return (
+    <ImageBackground
+      style={styles.img}
+      source={require('../../../Image/f1.png')}>
+      <View style={styles.container}>
+        <Image
+          style={styles.avatar}
+          source={require('../../../Image/testing.png')} />
+        <View style={styles.body}>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text> {PROFILE_FIELD.PROFILE}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text>{PROFILE_FIELD.FEEDBACK} </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text>{PROFILE_FIELD.ABOUT_US}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonContainer}
+            onPress={() => navigation.replace(NAVIGATION_SCREENS.LOGIN)}>
+            <Text>{PROFILE_FIELD.LOG_OUT}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 68,
+    borderWidth: 3,
+    borderColor: BACKGROUND_COLORS.BLACK,
+    marginBottom: "20%",
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 45,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: BACKGROUND_COLORS.ORANGE_,
+  }
+});
+
+
+
+
+
+
+
